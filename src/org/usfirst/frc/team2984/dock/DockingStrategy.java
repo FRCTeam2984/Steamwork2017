54ee98b458a4206179e738b97eabea2c80f1586f
Rich Apodaca <rich.apodaca@gmail.com>
2017-02-17 09:44:56 -0800

    Added DriveTrain and started tests.

--- src/org/usfirst/frc/team2984/dock/DockingStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team2984.dock;
-
-import java.util.List;
-
-import edu.wpi.first.wpilibj.command.Command;
-
-public interface DockingStrategy {
-	public List<Command> dock(VisionTarget target);
-}
